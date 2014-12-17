54ef5145e7812fa2080db40187bc89a349038e69
Gael Rehault <gaelrehault@users.noreply.github.com>
2014-12-17 20:54:26 +0000

    Update ssh.py
    bam !

--- auto_common/ssh.py
+++ auto_common/ssh.py
@@ -13,10 +13,6 @@ class Ssh():
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
             os = sys.platform
             
-            if "win" in os :
-                retstr= subprocess.check_output("del %HOMEPATH%\\.ssh\\known_hosts",stderr=subprocess.STDOUT, shell=True)
-            elif "linux" in os:
-                retstr= subprocess.check_output("ssh-keygen -R " + address,stderr=subprocess.STDOUT, shell=True)
             client.connect(address, username=usr, password=pwd)
             stdin, ss_stdout, ss_stderr = client.exec_command(command)
             r_out, r_err = ss_stdout.read(), ss_stderr.read()
