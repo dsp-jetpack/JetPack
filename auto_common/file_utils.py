06d4f0543db92527bbc8702d380ab60bdcbb6af8
gaelrehault <gael01@gmail.com>
2015-01-27 09:52:02 +0000

    fix for ks file update

--- auto_common/file_utils.py
+++ auto_common/file_utils.py
@@ -1,35 +1,36 @@
 import re
+import codecs
 
 class FileHelper():
-    
+
     @staticmethod
     def replaceExpression(fileref, searchExp,replaceExp):
-        fh = open(fileref, 'r')
-        content = fh.readlines() #Dont try this on large files..
+        fh = open(fileref, 'rU')
+        content = fh.readlines()
         fh.close()
         updated = []
-        
         for line in content:
             line = re.sub(searchExp,replaceExp, line)
             updated.append(line)
-         
-        f_out = file(fileref, 'wb')   
-        f_out.writelines(updated)
-        f_out.close()
-        
+
+        with codecs.open(fileref,'wbU',encoding='utf8') as f:
+            for line in updated:
+                f.write(line)
+
+
     @staticmethod
     def replaceExpressionTXT(fileref, searchExp,replaceExp):
         fh = open(fileref, 'r')
         content = fh.readlines() #Dont try this on large files..
         fh.close()
         updated = []
-         
+
         for line in content:
             line = re.sub(searchExp,replaceExp, line)
             updated.append(line)
-          
-        f_out = file(fileref, 'w')   
+
+        f_out = file(fileref, 'w')
         f_out.writelines(updated)
-        f_out.close()     
-        
-    
+        f_out.close()
+
+
