d93f50751da71af8179ea75bce4d0b62b79e82c0
jtwill98 <j_t_williams@dell.com>
2015-02-25 16:33:21 -0600

    add https port to iptables

--- src/mgmt/deploy-ceph-vm.sh
+++ src/mgmt/deploy-ceph-vm.sh
@@ -174,6 +174,7 @@ chvt 8
 -A INPUT -i lo -j ACCEPT
 -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 4505 -j ACCEPT
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 4506 -j ACCEPT
 -A INPUT -j REJECT --reject-with icmp-host-prohibited
