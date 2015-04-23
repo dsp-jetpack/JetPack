2a3dd4b51e47c2546726d8160ac434ac5938d8b8
rajinir <rajini_ram@dell.com>
2015-04-23 16:22:21 -0400

    Workaround https://bugzilla.redhat.com/show_bug.cgi?id=1192674 Fix is in puppet master upstream but not in A2 (or A3) bits. We are just pulling the fix proactively into solution thru scripts

--- src/utils/networking/hammer-configure-foreman.sh
+++ src/utils/networking/hammer-configure-foreman.sh
@@ -123,3 +123,9 @@ echo
 execute "hammer os set-parameter --operatingsystem-id ${OS_ID} --name subscription_manager_password --value \"${SUBSCRIPTION_MANAGER_PASSWORD}\""
 
 ./hammer-dump-ids.sh
+
+# Workaround for neutron fails to start, BZ1192674
+cp /usr/share/openstack-puppet/modules/neutron/manifests/server.pp /usr/share/openstack-puppet/modules/neutron/manifests/server.pp.orig
+
+sed -e "s/require*=> Neutron_config/subscribe     => Neutron_config/"  /usr/share/openstack-puppet/modules/neutron/manifests/server.pp.orig > /usr/share/openstack-puppet/modules/neutron/manifests/server.pp
+
