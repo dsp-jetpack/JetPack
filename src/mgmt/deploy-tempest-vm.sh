d79e1fc8eac61e75d4ab48d654be1acb18156ecb
waynepallen <Wayne_Allen@dell.com>
2015-05-19 11:48:33 -0500

    adding screen install

--- src/mgmt/deploy-tempest-vm.sh
+++ src/mgmt/deploy-tempest-vm.sh
@@ -223,7 +223,7 @@ EOIP
   systemctl disable firewalld
   systemctl disable chronyd
 
-  yum -y install python-devel python-pip python-crypto.x86_64 libxslt-devel libxml2-devel libffi-devel
+  yum -y install python-devel python-pip python-crypto.x86_64 libxslt-devel libxml2-devel libffi-devel screen
 
   cd /root 
   git clone https://github.com/redhat-openstack/tempest.git
