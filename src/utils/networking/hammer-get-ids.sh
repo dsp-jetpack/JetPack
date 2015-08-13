673a5753386ee392483b858939ab0a044768bbce
KevinCalman <kevin_calman@dell.com>
2015-08-13 13:13:07 -0500

    Update former Apache licenses to GPL3 (CES--3482)

--- src/utils/networking/hammer-get-ids.sh
+++ src/utils/networking/hammer-get-ids.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-# Copyright 2015, Dell
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# OpenStack - A set of software tools for building and managing cloud
+# computing platforms for public and private clouds.
+# Copyright (C) 2015 Dell, Inc.
 #
-#  http://www.apache.org/licenses/LICENSE-2.0
+# This file is part of OpenStack.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# OpenStack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Author:  Chris Dearborn
-# Version: 1.0
+# OpenStack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenStack.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 . ./common.sh
