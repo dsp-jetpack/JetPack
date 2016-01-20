2427d0c3f718b1ca2850c8d9cc463b598a25ec32
gaelrehault <gael01@gmail.com>
2016-01-20 10:17:12 -0600

    switched to dell-esg idracula fork

--- osp_deployer/director.py
+++ osp_deployer/director.py
@@ -216,8 +216,8 @@ class Director():
                 "echo 'export GOPATH=$HOME/go' >>$HOME/.bash_profile",
                 "echo 'export PATH=$PATH:$HOME/go/bin' >> $HOME/.bash_profile",
                 'sudo yum -y install golang -y',
-                '. $HOME/.bash_profile;go get github.com/VictorLowther/idracula',
-                '. $HOME/.bash_profile;go install github.com/VictorLowther/idracula'
+                '. $HOME/.bash_profile;go get get github.com/dell-esg/idracula',
+                '. $HOME/.bash_profile;go install github.com/dell-esg/idracula'
                 ]
             for cmd in cmds:
                 logger.info( Ssh.execute_command_tty(self.settings.director_node.external_ip, self.settings.director_install_account_user, self.settings.director_install_account_pwd,cmd))
