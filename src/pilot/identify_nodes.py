71243860895d00794d3dfa7604e0266076dda873
asbishop <alan_bishop@dell.com>
2016-02-11 10:25:28 -0500

    New tool for Director to help User identify overcloud nodes
    identify_nodes.py is a new Director tool that displays the iDRAC IP address
    for every overcloud node. This tells you which node is running on which piece
    of hardware.

---
+++ src/pilot/identify_nodes.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import ironicclient
+from novaclient import client as novaclient
+from os import path
+from subprocess import check_output
+
+
+def get_openstack_creds():
+    creds_file = open(path.join(path.expanduser('~'), 'stackrc'), 'r')
+
+    for line in creds_file:
+        prefix = "export"
+        if line.startswith(prefix):
+            line = line[len(prefix):]
+
+        line = line.strip()
+        key, val = line.split('=', 2)
+        key = key.lower()
+
+        if key == 'os_username':
+            os_username = val
+        elif key == 'os_auth_url':
+            os_auth_url = val
+        elif key == 'os_tenant_name':
+            os_tenant_name = val
+
+    os_password = check_output(['sudo', 'hiera', 'admin_password']).strip()
+
+    return os_auth_url, os_tenant_name, os_username, os_password
+
+
+def main():
+    os_auth_url, os_tenant_name, os_username, os_password = \
+        get_openstack_creds()
+
+    kwargs = {'os_username': os_username,
+              'os_password': os_password,
+              'os_auth_url': os_auth_url,
+              'os_tenant_name': os_tenant_name}
+    ironic = ironicclient.client.get_client(1, **kwargs)
+    nodes = ironic.node.list(detail=True)
+
+    nova = novaclient.Client('2',  # API version
+                             os_username,
+                             os_password,
+                             os_tenant_name,
+                             os_auth_url)
+
+    # Slightly odd syntax for declaring 'banner' reduces the line length
+    banner = (
+        "+-----------------+---------------------------+-----------------+"
+    )
+    nodeinfo = "| {:<15} | {:<25} | {:<15} |"
+    print banner
+    print nodeinfo.format('iDRAC Addr', 'Node Name', 'Provision Addr')
+    print banner
+    # Display the list ordered by the iDRAC address
+    for n in sorted(nodes, key=lambda x: x.driver_info['ipmi_address']):
+        idrac_address = n.driver_info['ipmi_address']
+        node_name = n.instance_info['display_name']
+        nova_ips = nova.servers.ips(n.instance_uuid)
+        if 'ctlplane' in nova_ips:
+            prov_addr = nova_ips['ctlplane'][0]['addr']
+        else:
+            prov_addr = 'None'
+        print nodeinfo.format(idrac_address, node_name, prov_addr)
+    print banner
+
+
+if __name__ == "__main__":
+    main()
