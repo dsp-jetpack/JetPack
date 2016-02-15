470240aca3c31375ec2181efe31a466b9f417e76
mikeyp-dell <michael_pittaro@dell.com>
2016-02-15 08:58:03 -0800

    disable netperf test
    Disabled the netperftest because it has a dependency on importlib, which is not
    available in Python 2.6.  Will restore properly later on.

--- network_performance/network_testing/__init__.py
+++ network_performance/network_testing/__init__.py
@@ -1,11 +1,13 @@
 import netvaltest
-import netperftest
+#import netperftest
 
 
 def run_netvaltest():
     """ entry point for network validation tool """    
     netvaltest.Main()
 
-def run_netperftest():
-    """ entry point for network performance tool """    
-    netperftest.Main()
+# XXX(mikeyp) disable since netperftest depends on importlib, which is not in 
+# Python 2.6
+#def run_netperftest():
+#    """ entry point for network performance tool """    
+#    netperftest.Main()
