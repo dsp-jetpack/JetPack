57362d5ad67bcf27138355787510700e764c5a5e
mikeyp-dell <michael_pittaro@dell.com>
2016-02-25 10:14:41 -0800

    add hardcoded exmaple of running a testcase
    netvaltest has been updated to include an example of running a single test
    using the new API's.  Most argument processing is temporarily disabled.

--- network_performance/network_testing/netvaltest.py
+++ network_performance/network_testing/netvaltest.py
@@ -1,22 +1,20 @@
 import argparse
 import datetime
-# from optparse import OptionParser
 import os
-
-import time
 import random
 import re
+import time
 
 import netlog
-
 from iperf3 import Iperf3Server, Iperf3Client
 # from iperf3 import Iperf3IntervalResult
 
 def getResults(server_node, client_node, param_list, output, direction,
                expected_rate, rate_window, test_time, run_time):
 
+    # XXX A mixture of result collection and validation.
+    # This can be likely be replaced with a per node test result check 
     """
-    Validate the connections between nodes.
 
     Keyword arguments:
 
@@ -95,7 +93,7 @@ def getResults(server_node, client_node, param_list, output, direction,
             + str(direction) + '\t\t' + str(bandwidth[-1]) + '\t' + result\
             + '\t' + time_result + '\t\t' + mode
 
-def commandGenerator(n1, n2):
+def commandGenerator(n1, n2, arguments):
 
     """
     Validate the connections between nodes.
@@ -107,7 +105,6 @@ def commandGenerator(n1, n2):
 
     """
     netlog.debug('commandGenerator()')
-    params = ['', '-R']
     # cmds = []
     cmds_list = []
     # run_list = []
@@ -121,31 +118,30 @@ def commandGenerator(n1, n2):
     for n in n2:
         for c in n:
             client_list.append(c)
-    # print server_list
-    # print client_list
-    # netlog.debug("Generating commands for: " + str(n1) + ' ' + str(n2) )
+    netlog.debug("Generating commands for: " + str(n1) + ' ' + str(n2) )
 
     for server in server_list:
         for client in client_list:
-            for p in params:
-                if p == '-R':
-                    direction = 'reverse'
-                    cmd = 'iperf3 -c ' + str(server) + ' ' + str(p)
-                else:
-                    direction = 'forward'
-                    cmd = 'iperf3 -c ' + str(server)
+            if arguments.reverse:
+                direction = 'reverse'
+                cmd = 'iperf3 -c ' + str(server) + ' ' + str(p)
+            else:
+                direction = 'forward'
+                cmd = 'iperf3 -c ' + str(server)
 
-                if server_list.index(server) == client_list.index(client):
-                    pair = True
-                else:
-                    pair = False
-                port = 5000 + int(server_list.index(server)*100)\
-                    + int(client_list.index(client)*10) + int(params.index(p))
-   
-                cmds_list.append({'server': server, 'client': client,
-                                  'command': cmd, 'is_pair': pair,
-                                  'direction': direction, 'port_number': port})
-    # netlog.debug("Command generation complete, generated " + str(len(cmds_list)) + " commands")
+            if server_list.index(server) == client_list.index(client):
+                pair = True
+            else:
+                pair = False
+            #port = 5000 + int(server_list.index(server)*100)\
+            #    + int(client_list.index(client)*10) + int(params.index(p))
+            # XXX need to revisit port assignment logic
+            port = 5000 + int(server_list.index(server)*100)
+
+            cmds_list.append({'server': server, 'client': client,
+                              'command': cmd, 'is_pair': pair,
+                              'direction': direction, 'port_number': port})
+    netlog.debug("Command generation complete, generated " + str(len(cmds_list)) + " commands")
     return cmds_list
 
 def Main():
@@ -168,31 +164,57 @@ def Main():
     parser = argparse.ArgumentParser()
 
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-f", "--forward", action="store_true")
-    group.add_argument("-R", "--reverse", action="store_true")
-    group.add_argument("-b", "--both", action="store_true")
+    group.add_argument("-f", "--forward", 
+        action="store_true", 
+        default=True)
+    group.add_argument("-r", "--reverse", 
+        action="store_true", 
+        default=False)
+    group.add_argument("-b", "--both",
+        action="store_true", 
+        default=False)
 
     group2 = parser.add_mutually_exclusive_group()
-    group2.add_argument("-s", "--sequential", action="store_true")
-    group2.add_argument("-c", "--concurrent", action="store_true")
+    group2.add_argument("-s", "--sequential", 
+        action="store_true", 
+        default=True)
+    group2.add_argument("-c", "--concurrent", 
+        action="store_true", 
+        default=False)
 
     group3 = parser.add_mutually_exclusive_group()
-    group3.add_argument("-a", "--allpermutations", action="store_true")
-    group3.add_argument("-p", "--pairs", action="store_true")
+    group3.add_argument("-a", "--allpermutations", 
+        action="store_true")
+    group3.add_argument("-p", "--pairs", 
+        action="store_true")
 
-    parser.add_argument("-e", "--expected_rate", type=int,
-                        help="expected rate")
-    parser.add_argument("-w", "--rate_window", type=int, help="window rate")
-    parser.add_argument("-t", "--test_time", type=int, help="test time")
+    parser.add_argument("-e", "--expected_rate", 
+        type=int,
+        default=0,
+        help="expected rate")
+    parser.add_argument("-w", "--rate_window", 
+        type=int, 
+        default=0,
+        help="window rate")
+    parser.add_argument("-t", "--test_time", 
+        type=int, 
+        default=10,
+        help="test time")
 
-    parser.add_argument('--node_list1', action='append',
-                        nargs='+', required=True,
-                        help='first list of nodes to test')
-    parser.add_argument('--node_list2', action='append', nargs='+',
-                        required=True,
-                        help='second list of nodes to test')
-    parser.add_argument("-v", "--verbose", help="increase output verbosity",
-                    action="store_true")
+    parser.add_argument('--node_list1', 
+        action='append',
+        nargs='+', 
+        required=True,
+        help='first list of nodes to test')
+    parser.add_argument('--node_list2', 
+        action='append', 
+        nargs='+',
+        required=True,
+        help='second list of nodes to test')
+    parser.add_argument("-v", "--verbose", 
+        help="increase output verbosity",
+        action="store_true")
+
     # with just 2 nodes the concurrent/sequential
     # /allperm options are irrelevant.
 
@@ -201,93 +223,78 @@ def Main():
         netlog.enable_debug() 
     # catching all invalid argument combinations.
 
+    # XXX should this code logging arguments / operation mode be in a 
+    # log_arguments function ? Probably
+
     if args.allpermutations:
-        #logging.basicConfig(level=logging.DEBUG)
         netlog.info("you are running all-permutations of tests across"
                " nodes: " + str(args.node_list1) + ' and ' + str(args.node_list2))
-        param_list.append('-a')
     elif args.pairs:
         netlog.info("you are running pairs of tests across"
                " nodes: " + str(args.node_list1) + 'and ' + str(args.node_list2))
-        param_list.append('-p')
     else:
         netlog.info('no permutations set, running in pair mode')
-        param_list.append('-p')
 
     if args.concurrent:
         netlog.info("you are running tests concurrently")
-        param_list.append('-c')
-        # mode = 'Concurrent'
-
     elif args.sequential:
         netlog.info("you are running tests sequentially.")
-        param_list.append('-s')
-        # mode = 'Sequential'
     else:
         netlog.info('no Order parameter set')
-        # print 'no Order parameter set'
-        # mode = 'Sequential'
 
     if args.reverse:
         netlog.info("Testing connectivity in reverse direction.")
-        # print ("Testing connectivity in reverse direction.")
-        param_list.append('-R')
-        # direction = 'Reverse'
     elif args.both:
         netlog.info("Testing connectivity in both directions.")
-        # print ("Testing connectivity in both directions.")
-        param_list.append('-b')
-        # direction = 'Both'
     elif args.forward:
         netlog.info("Testing connectivity in forward direction.")
-        # print ("Testing connectivity in forward direction.")
-        param_list.append('-f')
-        # direction = 'forward'
     else:
         netlog.info("Testing connectivity in forward direction.")
-        # print ("Testing connectivity in forward direction.")
-        # param_list.append('-f')
-        # direction = 'forward'
 
-    # setting the defaults for metrics if they are not entered.
-    if args.expected_rate:
-        netlog.info("Expected Rate: " + str(args.expected_rate))
-        # print ("Expected Rate: " + str(args.expected_rate))
-        metrics_list.append({'-e': args.expected_rate})
-        expected_rate = args.expected_rate
-    else:
-        expected_rate = 0.0
+    netlog.info("Expected Rate: " + str(args.expected_rate))
+    netlog.info("rate window: " + str(args.rate_window))
+    netlog.info("test_time: " + str(args.test_time))
 
-    if args.rate_window:
-        netlog.info("rate window: " + str(args.rate_window))
-        # print ("rate window: " + str(args.rate_window))
-        metrics_list.append({'-w': args.rate_window})
-        rate_window = args.rate_window
-    else:
-        rate_window = 0
+    # Build the list of iperf tests to execute
+    """
+    pairs:  
+        check both server lists are equal length
+        run test between list1[n] and list2[n]
+            do forward or reverse or both
+            David thought we should do all forwards before doing reverses
+            should warn if nodes are the same, but not skip
 
-    if args.test_time:
-        netlog.info("test_time: " + str(args.test_time))
-        # print ("test_time: " + str(args.test_time))
-        metrics_list.append({'-t': args.test_time})
-        test_time = args.test_time
-    else:
-        test_time = 60
+    
+    permutation
+        for each server in list 1:
+            for each server in list 2:
+                run test list1[n] - list2[n]
+                filter out cases already run
+                do forward or reverse or both
 
     # filtering the command matrix using the param_list to
     # only run selected jobs
 
+    """
+
     server_list = args.node_list1
     client_list = args.node_list2
-    run_list = commandGenerator(server_list, client_list)
+    run_list = commandGenerator(server_list, client_list, args)
     netlog.debug("Command generation complete, generated " + str(len(run_list)) + " commands")
+    netlog.debug(str(run_list))
     exec_list = []
 
+    # temporarily disable argument checks 
+    """
+
+    #if (args.both and args.allpermutations) or
+    #    (args.allpermutations and not args.reverse)) 
     if ('-b' in param_list and '-a' in param_list) or\
         ('-a' in param_list and ('-R' not in param_list
          and '-f' not in param_list)):
             exec_list = run_list
             # print 'run the entire list.'
+
     elif '-b' in param_list or ('-b' in param_list and '-p' in param_list):
         # print '-b or -b and -p'
         for each in run_list:
@@ -335,14 +342,26 @@ def Main():
         netlog.error('only one job, cannot run concurrently.')
         assert False, 'only one job, cannot run concurrently.'
 
-    if '-c' in param_list:
+    if args.concurrent: 
         # run in concurrent mode.
+        netlog.info('Running in concurrent  mode')
         assert False, 'concurrent not implemented'
     else:
         # sequential mode
         netlog.info('Running in Sequential mode')
-
+    """
         
+    # New Logic
+    # create IPerf3 server and client for each test to run
+    # in sequential mode, run one by one and wait.
+    # in parallel mode start all and wait
+
+    testcase = {
+        'server':'localhost',
+        'port_number': 5201,
+        'client' : 'localhost'
+    }
+    exec_list.append(testcase)
     print exec_list
     for linktest in exec_list:
 
@@ -364,6 +383,8 @@ def Main():
             client.parse_results()
             print client.final_recv_data
 
-        # TODO need to stop server here
+        # stop the server, and wait for the thread to complete
+        server.stop()
+        server.join(10)
                     
     netlog.info('Done')
