9a29464efe39f4ac087f057e9756d4d4eb261b7c
Richard Pioso <richard_pioso@dell.com>
2016-05-10 18:11:30 -0400

    CES-3489 Add and correct licensing headers (continued)

--- src/pilot/deployment-validation/sanity_test.sh
+++ src/pilot/deployment-validation/sanity_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# (c) 2016 Dell
+# (c) 2015-2016 Dell
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
