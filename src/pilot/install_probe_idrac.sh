9fa4b8a8b612171d490893857651af3a221eaf70
Gael Rehault <root@sah.cwdg720xd01.org>
2016-09-07 14:52:23 -0500

    CES-6332

--- src/pilot/install_probe_idrac.sh
+++ src/pilot/install_probe_idrac.sh
@@ -14,21 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cd /tmp
-
-# Obtain and install the Extra Packages for Enterprise Linux (EPEL) repository.
-# That repo contains the python-pip package, which is required by this script.
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum -y install ./epel-release-latest-7.noarch.rpm
-
-# Ensure that packages that are offered by both EPEL and a Red Hat Enterprise
-# Linux (RHEL) repository are obtained from the RHEL repo.  This is accomplished
-# by setting the priority of the EPEL repository to be a greater value than the
-# priorities of the RHEL repositories.
-sudo yum-config-manager --enable epel --setopt="epel.priority=2"
-
 # Install the python-pip package.
-sudo yum install -y python-pip
+sudo easy_install pip
 
 cd ~/pilot/probe_idrac
 
