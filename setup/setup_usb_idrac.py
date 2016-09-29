44d22dfa454136a16a25bb452df21f6a9844d439
Gael Rehault <gaelrehault@users.noreply.github.com>
2016-09-29 09:01:59 +0100

    Ces 6508 (#238)
    * added support for physical usb key to install the SAH node
    
    * updated readme with pointer to deployment guide

--- setup/setup_usb_idrac.py
+++ setup/setup_usb_idrac.py
@@ -27,7 +27,7 @@ from osp_deployer.sah import Sah
 logger = logging.getLogger("osp_deployer")
 
 
-def setup_networking():
+def setup():
     try:
         hdlr = logging.FileHandler('setup_usb_idrac.log')
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -43,7 +43,23 @@ def setup_networking():
         parser.add_argument('-s', '--settings',
                             help='ini settings file, e.g settings/acme.ini',
                             required=True)
+        parser.add_argument('-usb_key', '--usb_key',
+                            help='Use a physical USB key - device to use ' +
+                            ' eg : -usb_key /dev/sdb',
+                            required=False)
+        parser.add_argument('-idrac_vmedia_img', '--idrac_vmedia_img',
+                            help='Use an idrac virtual media image',
+                            action='store_true', required=False)
+
         args, ignore = parser.parse_known_args()
+
+        if args.usb_key is None and args.idrac_vmedia_img is False:
+            raise AssertionError("You need to spefify the type of" +
+                                 " installation to perform \n" +
+                                 "-usb_key devideID if using a " +
+                                 "physical key\n" +
+                                 "-idrac_vmedia_img if using " +
+                                 "an idrac virtual media image")
 
         logger.debug("loading settings files " + args.settings)
         settings = Settings(args.settings)
@@ -62,22 +78,37 @@ def setup_networking():
                                                stderr=subprocess.STDOUT,
                                                shell=True).strip()
         target_ini = settings.settings_file.replace(current_path, "/mnt/usb")
-        cmds = ['cd ~;rm -f osp_ks.img',
-                'cd ~;dd if=/dev/zero of=osp_ks.img bs=1M count=5000',
-                'cd ~;mkfs ext3 -F osp_ks.img',
-                'mkdir -p /mnt/usb',
-                'cd ~;mount -o loop osp_ks.img /mnt/usb',
-                'cd ~;cp -R ~/JetStream /mnt/usb',
-                "sed -i 's|" + current_path + "|/root|' " + target_ini,
-                'sync; umount /mnt/usb']
+
+        if args.idrac_vmedia_img is True:
+            cmds = ['cd ~;rm -f osp_ks.img',
+                    'cd ~;dd if=/dev/zero of=osp_ks.img bs=1M count=5000',
+                    'cd ~;mkfs ext3 -F osp_ks.img',
+                    'mkdir -p /mnt/usb',
+                    'cd ~;mount -o loop osp_ks.img /mnt/usb',
+                    'cd ~;cp -R ~/JetStream /mnt/usb',
+                    "sed -i 's|" + current_path + "|/root|' " + target_ini,
+                    'sync; umount /mnt/usb']
+        else:
+            cmds = ['mkfs.ext3 -F ' + args.usb_key,
+                    'mkdir -p /mnt/usb',
+                    'cd ~;mount -o loop ' + args.usb_key +
+                    ' /mnt/usb',
+                    'cd ~;cp -R ~/JetStream /mnt/usb',
+                    "sed -i 's|" + current_path + "|/root|' " + target_ini,
+                    'sync; umount /mnt/usb']
+
         for cmd in cmds:
             logger.debug("running " + cmd)
             logger.debug(subprocess.check_output(cmd,
                                                  stderr=subprocess.STDOUT,
                                                  shell=True))
 
-        logger.info("All done - attach ~/osp_ks.img to the sah node" +
-                    " & continue with the deployment ...")
+        if args.idrac_vmedia_img:
+            logger.info("All done - attach ~/osp_ks.img to the sah node" +
+                        " & continue with the deployment ...")
+        else:
+            logger.info("All done - plug the usb into the sah node" +
+                        " & continue with the deployment ...")
 
     except:
         logger.error(traceback.format_exc())
@@ -88,4 +119,4 @@ def setup_networking():
 
 
 if __name__ == "__main__":
-    setup_networking()
+    setup()
