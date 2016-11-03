8dc503b7c8a3ecb415ec7bac9cf5668be55b4926
cdearborn <Christopher_Dearborn@dell.com>
2016-11-03 16:08:14 -0400

    Moved vendor passthru patching to before assign_role.

--- src/pilot/install-director.sh
+++ src/pilot/install-director.sh
@@ -128,5 +128,16 @@ echo "## Updating .bash_profile..."
 echo "source ~/stackrc" >> ~/.bash_profile
 echo "## Done."
 
+# This hacks in a patch that has been merged to upstream already, but is not
+# currently present in OSP10.  We will need to remove this after the fix
+# appears in OSP10.
+echo
+echo "## Patching Ironic vendor passthru..."
+OUT=$(sudo patch -b -s /usr/lib/python2.7/site-packages/ironic/drivers/modules/drac/vendor_passthru.py ~/pilot/vendor_passthru.patch)
+sudo rm -f /usr/lib/python2.7/site-packages/ironic/drivers/modules/drac/vendor_passthru.pyc
+sudo rm -f /usr/lib/python2.7/site-packages/ironic/drivers/modules/drac/vendor_passthru.pyo
+sudo systemctl restart openstack-ironic-conductor.service
+echo "## Done."
+
 echo
 echo "## Configuration complete!"
