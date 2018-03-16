136994810b4b9ef4947ad5ac2572c2201c7453c8
jproque-dell <jeanpierre.roquesalane@dell.com>
2018-03-16 16:16:58 +0100

    Removing NovaCpuPinSet and NovaSchedulerDefaultFIlter management from Puppet Manifests. Now handle with TripleO params

--- src/pilot/templates/dellnfv/hugepages.pp
+++ src/pilot/templates/dellnfv/hugepages.pp
@@ -26,14 +26,6 @@ class dellnfv::hugepages (
 ) {
   if ($step >= 4){
     if ($state == true) {
-        #Create log file for hugepage
-        file {'/tmp/dellnfv_hpg_config.log':
-                ensure => 'present',
-                content => 'FAIL : HugePages configuration failed',
-                owner => 'heat-admin',
-                group => 'heat-admin',
-                mode => 0777,
-        }
 
         #update the kernel using hugepages details, hugepage default size is equal to hugepage size
         exec {'update_kernel_hugepages':
@@ -41,14 +33,6 @@ class dellnfv::hugepages (
                 command => "sudo grubby --update-kernel=ALL --args=\"default_hugepagesz=$hpg_size hugepagesz=$hpg_size hugepages=$hpg_number\"",
         }
 
-        #Update in-built log file for SUCCESS
-        file_line {'edit_success_hugepage':
-                path => '/tmp/dellnfv_hpg_config.log',
-                line => 'SUCCESS : HugePages configuration successful',
-                match => "FAIL : HugePages configuration failed",
-                replace => true,
-                require => Exec['update_kernel_hugepages'],
-        }
     }
   }
 }
--- src/pilot/templates/dellnfv/numa.pp
+++ src/pilot/templates/dellnfv/numa.pp
@@ -25,37 +25,12 @@ class dellnfv::numa (
   if ($step>=4) {
         if ($state == true){
 
-        #Create log file for NUMA
-        file {'/tmp/dellnfv_numa_config.log':
-                ensure => 'present',
-                content => 'FAIL : CPU Pinning configuration failed',
-                owner => 'heat-admin',
-                group => 'heat-admin',
-                mode => 0777,
-        }
-
         #update the kernel using isolcpus
         exec {'update_kernel_numa':
 				path => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 				command => "sudo grubby --update-kernel=ALL --args=\"isolcpus=$vcpu_pin_set\"",
         }
 
-        #update vcpu_pin_set in nova.conf
-        exec {'set_vcpu_in_nova_conf':
-				require => Exec['update_kernel_numa'],
-				path => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-				command => "sudo crudini --set /etc/nova/nova.conf DEFAULT vcpu_pin_set \"${vcpu_pin_set}\"",
-
-        }
-
-        #Update in-built puppet log for SUCCESS
-        file_line {'edit_success_numa':
-				path => '/tmp/dellnfv_numa_config.log',
-				line => 'SUCCESS : CPU Pinning configuration successful',
-				match => "FAIL : CPU Pinning configuration failed",
-				replace => true,
-				require => Exec['set_vcpu_in_nova_conf'],
-        }
      }
   }
 }
