b9289d94edabf569a0cb1bf12a35e51edc904a92
Patil <Uday_Patil1@Dellteam.com>
2018-04-05 14:08:15 +0530

    Added Licenses

--- src/pilot/cpu_siblings.py
+++ src/pilot/cpu_siblings.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2016-2018 Dell Inc. or its subsidiaries.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
 # It is a dictionary of the form {'total number of cpu cores' : {'number_of_host_os_cpu' : {'vcpu_pin_set' : 'Range of corresponding cpus', 'host_os_cpu' : 'Range of corresponding cpus'} } }
 # If 'total number of cpu cores' = 48 & 'number_of_host_os_cpu' = 4 then you can get 'vcpu_pin_set' by using sibling_info[48][4]['vcpu_pin_set']
 sibling_info = {
