1c894f4cd8514d2ecacac4cb046943cc20d0e153
digambar15 <digambarpat@gmail.com>
2018-05-15 21:22:32 +0530

    modify changes

--- src/deploy/osp_deployer/profile.py
+++ src/deploy/osp_deployer/profile.py
@@ -151,10 +151,10 @@ class Profile():
 
     def is_in_range(self, value, input):
         valid = True
-        val_list = input.split('-')
-        min_val = int(val_list[0])
-        max_val = int(val_list[1]) + 1
-        if int(value) not in range(min_val, max_val):
+        list = input.split('-')
+        min = int(list[0])
+        max = int(list[1]) + 1
+        if int(value) not in range(min, max):
             valid = False
         return valid
 
