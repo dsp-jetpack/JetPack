b2106b95eafe60aaf1b75c4e194c104c17b0ea03
digambar15 <digambarpat@gmail.com>
2018-06-11 14:26:10 +0530

    Fix pep8 issues

--- src/pilot/assign_role.py
+++ src/pilot/assign_role.py
@@ -410,7 +410,7 @@ def define_storage_logical_disks(drac_client, raid_controller_name):
         return None
 
     os_logical_disk = define_storage_operating_system_logical_disk(
-            raid_cntlr_physical_disks, drac_client, raid_controller_name)
+        raid_cntlr_physical_disks, drac_client, raid_controller_name)
 
     if os_logical_disk is None:
         return None
@@ -461,7 +461,7 @@ def define_storage_operating_system_logical_disk(physical_disks, drac_client,
             os_logical_disk_size_gb,
             '\n  '.join(os_physical_disk_names)))
     if is_boss_controller(raid_controller_name, drac_client):
-        os_logical_disk_size_gb=0
+        os_logical_disk_size_gb = 0
     os_logical_disk = define_logical_disk(
         os_logical_disk_size_gb,
         '1',
