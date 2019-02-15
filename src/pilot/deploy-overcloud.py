e4d6eed5cf4bfd7d8aa6c41f573b966d49a3b287
Rajini Karthik <rajini.karthik@dell.com>
2019-02-15 06:27:34 -0600

    Support for Unity Cinder Backend (#251)
    Added automation support for unity cinder backend

--- src/pilot/deploy-overcloud.py
+++ src/pilot/deploy-overcloud.py
@@ -154,7 +154,7 @@ def create_volume_types():
     if not args.disable_rbd:
         types.append(["rbd_backend", "tripleo_ceph"])
 
-    if args.enable_dellsc:
+    if args.enable_dellsc || args.enable_unity :
         cinder_file = open(home_dir +
                            '/pilot/templates/dell-cinder-backends.yaml', 'r')
         for line in cinder_file:
@@ -305,6 +305,10 @@ def main():
                             action='store_true',
                             default=False,
                             help="Enable cinder Dell Storage Center backend")
+        parser.add_argument('--enable_unity',
+                            action='store_true',
+                            default=False,
+                            help="Enable Dell EMC Unity backend")
         parser.add_argument('--disable_rbd',
                             action='store_true',
                             default=False,
@@ -496,6 +500,9 @@ def main():
 
         if args.enable_dellsc:
             env_opts += " -e ~/pilot/templates/dell-cinder-backends.yaml"
+        
+       if args.enable_unity:
+            env_opts += " -e ~/pilot/templates/dellemc-unity-cinder-backends.yaml" 
 
         cmd = "cd ;source ~/stackrc; openstack overcloud deploy" \
               " {}" \
