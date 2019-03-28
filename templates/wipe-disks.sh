a414f3ea50e70ca233cfd9ee310113d77978d7d1
jproque-dell <jeanpierre.roquesalane@dell.com>
2019-03-28 11:49:08 -0500

    Adding templates, kickstart and script to erase ceph previous signature

---
+++ templates/wipe-disks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ `hostname` = *"hci"* ]]
+then
+  echo "Number of disks detected: $(lsblk -no NAME,TYPE,MOUNTPOINT | grep "disk" | awk '{print $1}' | wc -l)"
+  for DEVICE in `lsblk -no NAME,TYPE,MOUNTPOINT | grep "disk" | awk '{print $1}'`
+  do
+    ROOTFOUND=0
+    echo "Checking /dev/$DEVICE..."
+    echo "Number of partitions on /dev/$DEVICE: $(expr $(lsblk -n /dev/$DEVICE | awk '{print $7}' | wc -l) - 1)"
+    for MOUNTS in `lsblk -n /dev/$DEVICE | awk '{print $7}'`
+    do
+      if [ "$MOUNTS" = "/" ]
+      then
+        ROOTFOUND=1
+      fi
+    done
+    if [ $ROOTFOUND = 0 ]
+    then
+      echo "Root not found in /dev/${DEVICE}"
+      echo "Wiping disk /dev/${DEVICE}"
+      sgdisk -Z /dev/${DEVICE}
+      sgdisk -g /dev/${DEVICE}
+      dmsetup remove_all
+    else
+      echo "Root found in /dev/${DEVICE}"
+    fi
+  done
+fi
