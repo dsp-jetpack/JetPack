735f13afdf6fad9076bb998db3828e20d81237d4
MUHAMMAD AHMAD <root@sah.oss.labs>
2020-08-31 11:04:43 +0000

    SRIOV-Offload support, updates and templates

--- src/deploy/osp_deployer/director.py
+++ src/deploy/osp_deployer/director.py
@@ -232,7 +232,7 @@ class Director(InfraHost):
             cmd = '~/pilot/install-director.sh --dns ' + \
                   self.settings.name_server + \
                   " --director_ip " + \
-                  self.ip 
+                  self.ip
 
         if len(self.settings.overcloud_nodes_pwd) > 0:
             cmd += " --nodes_pwd " + self.settings.overcloud_nodes_pwd
@@ -1543,27 +1543,16 @@ class Director(InfraHost):
         # Specify number of VFs for sriov mentioned interfaces
         sriov_map_setting = []
         sriov_pci_passthrough = []
-        physical_network = ["physint", "physint1", "physint2", "physint3"]
+        physical_network = "physint"
         check = 0
         for interface in sriov_interfaces:
-            devname = interface
-            if (self.settings.enable_smart_nic is True):
-                mapping = physical_network[check] + ':' + interface
-                nova_pci = '{devname: ' + \
-                           '"' + interface + '",' + \
-                           'physical_network: ' + \
-                           '"' + physical_network[check] + '"}'
-                check = check + 1
-
-            else:
-                mapping = physical_network[0] + ':' + interface
-                nova_pci = '{devname: ' + \
-                           '"' + interface + '",' + \
-                           'physical_network: ' + \
-                           '"' + physical_network[0] + '"}'
+            mapping = physical_network + ':' + interface
+            nova_pci = '{devname: ' + \
+                       '"' + interface + '",' + \
+                       'physical_network: ' + \
+                       '"' + physical_network + '"}'
 
             sriov_map_setting.append(mapping)
-
             sriov_pci_passthrough.append(nova_pci)
 
         sriov_map_setting = "'" + ",".join(sriov_map_setting) + "'"
