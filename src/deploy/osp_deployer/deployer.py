781a829d5271e4061040f3be81cb8353a78c3dfb
root <root@sah.r180.dsp.labs>
2020-11-22 09:55:31 -0600

    .:

--- src/deploy/osp_deployer/deployer.py
+++ src/deploy/osp_deployer/deployer.py
@@ -233,15 +233,14 @@ def validate_edge_sites_in_settings(args, setts):
 
 
 def deploy_powerflex_gw(setts, sah_node, tester, powerflexgw_vm):
-    settings = setts
     powerflexgw_ip = setts.powerflexgw_vm.public_api_ip
     Ssh.execute_command(powerflexgw_ip,
                         "root",
-                        settings.powerflexgw_vm.root_password,
+                        setts.powerflexgw_vm.root_password,
                         "subscription-manager remove --all")
     Ssh.execute_command(powerflexgw_ip,
                         "root",
-                         settings.powerflexgw_vm.root_password,
+                         setts.powerflexgw_vm.root_password,
                         "subscription-manager unregister")
     logger.info("=== deleting any existing powerflex gateway vm")
     sah_node.delete_powerflexgw_vm()
@@ -263,15 +262,14 @@ def deploy_powerflex_gw(setts, sah_node, tester, powerflexgw_vm):
 
 
 def deploy_powerflex_mgmt(setts, sah_node, tester, powerflexmgmt_vm):
-    settings = setts
     powerflexmgmt_ip = setts.powerflexmgmt_vm.public_api_ip
     Ssh.execute_command(powerflexmgmt_ip,
                         "root",
-                        settings.powerflexmgmt_vm.root_password,
+                        setts.powerflexmgmt_vm.root_password,
                         "subscription-manager remove --all")
     Ssh.execute_command(powerflexmgmt_ip,
                         "root",
-                        settings.powerflexmgmt_vm.root_password,
+                        setts.powerflexmgmt_vm.root_password,
                         "subscription-manager unregister")
     logger.info("=== deleting any existing powerflex presentation server vm")
     sah_node.delete_powerflexmgmt_vm()
